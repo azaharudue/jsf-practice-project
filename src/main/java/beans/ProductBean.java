2afb85d7c917200b8cbf27ba20e9381cd6c3044f
Azahar Hossain <azahar.hossain10@yahoo.com>
2021-07-13 00:28:26 +0200

    Filter and sorting in columns added, companies.xhtml file added

--- src/main/java/beans/ProductBean.java
+++ src/main/java/beans/ProductBean.java
@@ -12,6 +12,7 @@ import javax.inject.Named;
 
 import org.primefaces.PrimeFaces;
 import org.primefaces.event.RowEditEvent;
+import org.primefaces.model.FilterMeta;
 
 import main.java.daoImpl.CompanyDaoImpl;
 import main.java.daoImpl.ProductDaoImpl;
@@ -20,12 +21,13 @@ import main.java.entities.Product;
 
 @Named("basicView")
 @ViewScoped
-public class ProductBean implements Serializable
-{
+public class ProductBean implements Serializable {
 
 	private static final long serialVersionUID = -3816399998127217254L;
 
 	private final CompanyDaoImpl companyDAO;
+
+	private List<FilterMeta> filterBy;
 
 	private final ProductDaoImpl productDAO;
 
@@ -35,52 +37,82 @@ public class ProductBean implements Serializable
 
 	private List<Product> selectedProducts;
 
-	public ProductBean()
-	{
+	public ProductBean() {
 		System.out.println("basicView construct");
 
 		this.productDAO = new ProductDaoImpl();
 		this.companyDAO = new CompanyDaoImpl();
 	}
 
-	public void clearForm()
-	{
+	public void clearForm() {
 
 		this.selectedProduct = null;
 	}
 
-	public List<Product> getProducts()
-	{
+	public void deleteProduct() {
+		this.products.remove(this.selectedProduct);
+		this.productDAO.delete(this.selectedProduct);
+		this.selectedProduct = null;
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product Removed"));
+		PrimeFaces.current().ajax().update("growl", "formProduct:tblProduct");
+
+	}
+
+	public void deleteSelectedProducts() {
+
+		this.products.removeAll(this.selectedProducts);
+
+		for (final Product productToBeDeleted : this.selectedProducts)
+			this.productDAO.delete(productToBeDeleted);
+
+		this.selectedProducts = null;
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Products Removed"));
+
+		PrimeFaces.current().ajax().update("growl", "formProduct:tblProduct");
+		// PrimeFaces.current().executeScript("PF('dtProducts').clearFilters()");
+	}
+
+	public String getDeleteButtonMessage() {
+		if (this.hasSelectedProducts()) {
+			final int size = this.selectedProducts.size();
+			return size > 1 ? size + " products selected" : "1 product selected";
+		}
+
+		return "Delete";
+	}
+
+	public List<Product> getProducts() {
 		return this.products;
 	}
 
 	/**
 	 * @return the selectedProduct
 	 */
-	public Product getSelectedProduct()
-	{
+	public Product getSelectedProduct() {
 		return this.selectedProduct;
 	}
 
 	/**
 	 * @return the selectedProducts
 	 */
-	public List<Product> getSelectedProducts()
-	{
+	public List<Product> getSelectedProducts() {
 		return this.selectedProducts;
 	}
 
-	public void onRowCancel(final RowEditEvent<Product> event)
-	{
+	public boolean hasSelectedProducts() {
+		return (this.selectedProducts != null) && !this.selectedProducts.isEmpty();
+	}
+
+	public void onRowCancel(final RowEditEvent<Product> event) {
 
 		final FacesMessage mgs = new FacesMessage("Edit Cancelled", String.valueOf(event.getObject()));
 		FacesContext.getCurrentInstance().addMessage(null, mgs);
 		this.clearForm();
 	}
 
-	public void onRowEdit(final RowEditEvent<Product> event)
-	{
-		final Product updatedProduct = (Product) event.getObject();
+	public void onRowEdit(final RowEditEvent<Product> event) {
+		final Product updatedProduct = event.getObject();
 
 		this.productDAO.update(updatedProduct);
 
@@ -89,35 +121,30 @@ public class ProductBean implements Serializable
 
 	}
 
-	public void openNew()
-	{
+	public void openNew() {
 		this.selectedProduct = new Product();
 		this.selectedProduct.setCompany(new Company());
 	}
 
 	@PostConstruct
-	public void populateProductList()
-	{
+	public void populateProductList() {
 		this.products = this.productDAO.findAll();
+		this.filterBy = new ArrayList<>();
 
 	}
 
-	public String saveProduct()
-	{
-		try
-		{
-
+	public String saveProduct() {
+		try {
 			this.companyDAO.save(this.selectedProduct.getCompany());
 			this.productDAO.save(this.selectedProduct);
 
 			this.products.add(this.selectedProduct);
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product created!"));
 
-		}
-		catch (final Exception e)
-		{
+		} catch (final Exception e) {
 
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getLocalizedMessage(), "Product not created!"));
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(e.getLocalizedMessage(), "Product not created!"));
 
 		}
 
@@ -127,61 +154,22 @@ public class ProductBean implements Serializable
 	/**
 	 * @param products the products to set
 	 */
-	public void setProducts(final List<Product> products)
-	{
+	public void setProducts(final List<Product> products) {
 		this.products = products;
 	}
 
 	/**
 	 * @param selectedProduct the selectedProduct to set
 	 */
-	public void setSelectedProduct(final Product selectedProduct)
-	{
+	public void setSelectedProduct(final Product selectedProduct) {
 		this.selectedProduct = selectedProduct;
 	}
 
 	/**
 	 * @param selectedProducts the selectedProducts to set
 	 */
-	public void setSelectedProducts(final List<Product> selectedProducts)
-	{
+	public void setSelectedProducts(final List<Product> selectedProducts) {
 		this.selectedProducts = selectedProducts;
-	}
-
-	public String getDeleteButtonMessage()
-	{
-		if (this.hasSelectedProducts())
-		{
-			final int size = this.selectedProducts.size();
-			return size > 1 ? size + " products selected" : "1 product selected";
-		}
-
-		return "Delete";
-	}
-
-	public boolean hasSelectedProducts()
-	{
-		return (this.selectedProducts != null) && !this.selectedProducts.isEmpty();
-	}
-
-	public void deleteSelectedProducts()
-	{
-		this.products.removeAll(this.selectedProducts);
-		this.selectedProducts = null;
-
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Products Removed"));
-
-		PrimeFaces.current().ajax().update("growl", "formProduct:tblProduct");
-		// PrimeFaces.current().executeScript("PF('dtProducts').clearFilters()");
-	}
-
-	public void deleteProduct()
-	{
-		this.products.remove(this.selectedProduct);
-		this.selectedProduct = null;
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product Removed"));
-		PrimeFaces.current().ajax().update("growl", "formProduct:tblProduct");
-
 	}
 
 }
