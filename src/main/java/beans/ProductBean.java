be90ac17c790afbbfff2d30d5684e4b5febec894
unknown <azahar.hossain@deloma.de>
2021-07-12 18:08:58 +0200

    New resources added

--- src/main/java/beans/ProductBean.java
+++ src/main/java/beans/ProductBean.java
@@ -10,7 +10,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
-import org.primefaces.component.datatable.DataTable;
+import org.primefaces.PrimeFaces;
 import org.primefaces.event.RowEditEvent;
 
 import main.java.daoImpl.CompanyDaoImpl;
@@ -20,7 +20,8 @@ import main.java.entities.Product;
 
 @Named("basicView")
 @ViewScoped
-public class ProductBean implements Serializable {
+public class ProductBean implements Serializable
+{
 
 	private static final long serialVersionUID = -3816399998127217254L;
 
@@ -34,53 +35,53 @@ public class ProductBean implements Serializable {
 
 	private List<Product> selectedProducts;
 
-	public ProductBean() {
+	public ProductBean()
+	{
 		System.out.println("basicView construct");
 
 		this.productDAO = new ProductDaoImpl();
 		this.companyDAO = new CompanyDaoImpl();
 	}
 
-	public void clearForm() {
+	public void clearForm()
+	{
 
 		this.selectedProduct = null;
 	}
 
-	public void deleteProduct() {
-
-	}
-
-	public List<Product> getProducts() {
+	public List<Product> getProducts()
+	{
 		return this.products;
 	}
 
 	/**
 	 * @return the selectedProduct
 	 */
-	public Product getSelectedProduct() {
+	public Product getSelectedProduct()
+	{
 		return this.selectedProduct;
 	}
 
 	/**
 	 * @return the selectedProducts
 	 */
-	public List<Product> getSelectedProducts() {
+	public List<Product> getSelectedProducts()
+	{
 		return this.selectedProducts;
 	}
 
-	public void onRowCancel(final RowEditEvent<Product> event) {
+	public void onRowCancel(final RowEditEvent<Product> event)
+	{
 
 		final FacesMessage mgs = new FacesMessage("Edit Cancelled", String.valueOf(event.getObject()));
 		FacesContext.getCurrentInstance().addMessage(null, mgs);
 		this.clearForm();
 	}
 
-	public void onRowEdit(final RowEditEvent<Product> event) {
-		final DataTable dataTable = (DataTable) event.getSource();
-		Product updatedProduct = null;
+	public void onRowEdit(final RowEditEvent<Product> event)
+	{
+		final Product updatedProduct = (Product) event.getObject();
 
-		if (dataTable != null)
-			updatedProduct = (Product) dataTable.getRowData();
 		this.productDAO.update(updatedProduct);
 
 		final FacesMessage mgs = new FacesMessage("Product Edited", String.valueOf(event.getObject()));
@@ -88,19 +89,23 @@ public class ProductBean implements Serializable {
 
 	}
 
-	public void openNew() {
+	public void openNew()
+	{
 		this.selectedProduct = new Product();
 		this.selectedProduct.setCompany(new Company());
 	}
 
 	@PostConstruct
-	public void populateProductList() {
+	public void populateProductList()
+	{
 		this.products = this.productDAO.findAll();
 
 	}
 
-	public String saveProduct() {
-		try {
+	public String saveProduct()
+	{
+		try
+		{
 
 			this.companyDAO.save(this.selectedProduct.getCompany());
 			this.productDAO.save(this.selectedProduct);
@@ -108,35 +113,75 @@ public class ProductBean implements Serializable {
 			this.products.add(this.selectedProduct);
 			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product created!"));
 
-		} catch (final Exception e) {
-			FacesContext.getCurrentInstance().addMessage(null,
-					new FacesMessage(e.getLocalizedMessage(), "Product not created!"));
+		}
+		catch (final Exception e)
+		{
+
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(e.getLocalizedMessage(), "Product not created!"));
 
 		}
+
 		return null;
 	}
 
 	/**
-	 * @param products
-	 *            the products to set
+	 * @param products the products to set
 	 */
-	public void setProducts(final List<Product> products) {
+	public void setProducts(final List<Product> products)
+	{
 		this.products = products;
 	}
 
 	/**
-	 * @param selectedProduct
-	 *            the selectedProduct to set
+	 * @param selectedProduct the selectedProduct to set
 	 */
-	public void setSelectedProduct(final Product selectedProduct) {
+	public void setSelectedProduct(final Product selectedProduct)
+	{
 		this.selectedProduct = selectedProduct;
 	}
 
 	/**
-	 * @param selectedProducts
-	 *            the selectedProducts to set
+	 * @param selectedProducts the selectedProducts to set
 	 */
-	public void setSelectedProducts(final List<Product> selectedProducts) {
+	public void setSelectedProducts(final List<Product> selectedProducts)
+	{
 		this.selectedProducts = selectedProducts;
 	}
+
+	public String getDeleteButtonMessage()
+	{
+		if (this.hasSelectedProducts())
+		{
+			final int size = this.selectedProducts.size();
+			return size > 1 ? size + " products selected" : "1 product selected";
+		}
+
+		return "Delete";
+	}
+
+	public boolean hasSelectedProducts()
+	{
+		return (this.selectedProducts != null) && !this.selectedProducts.isEmpty();
+	}
+
+	public void deleteSelectedProducts()
+	{
+		this.products.removeAll(this.selectedProducts);
+		this.selectedProducts = null;
+
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Products Removed"));
+
+		PrimeFaces.current().ajax().update("growl", "formProduct:tblProduct");
+		// PrimeFaces.current().executeScript("PF('dtProducts').clearFilters()");
+	}
+
+	public void deleteProduct()
+	{
+		this.products.remove(this.selectedProduct);
+		this.selectedProduct = null;
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Product Removed"));
+		PrimeFaces.current().ajax().update("growl", "formProduct:tblProduct");
+
+	}
+
 }
